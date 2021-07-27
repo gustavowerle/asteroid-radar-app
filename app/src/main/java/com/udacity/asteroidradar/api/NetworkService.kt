ccfd5d56810576d74048b7dcace193887d568d7a
GustavoWerle01 <gustavo.l.werle@gmail.com>
2021-07-27 19:45:38 -0300

    Add an interceptor to put the API Key at all requests and refactor the local of API Key to improve the security

--- app/src/main/java/com/udacity/asteroidradar/api/NetworkService.kt
+++ app/src/main/java/com/udacity/asteroidradar/api/NetworkService.kt
@@ -2,10 +2,12 @@ package com.udacity.asteroidradar.api
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.Constants
 import com.udacity.asteroidradar.PictureOfDay
-import org.json.JSONObject
-import org.json.JSONStringer
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
@@ -17,14 +19,11 @@ interface AsteroidsRest {
     @GET("neo/rest/v1/feed")
     fun getAsteroids(
         @Query("start_date") startDate: String,
-        @Query("end_date") endDate: String,
-        @Query("api_key") apiKey: String = Constants.API_KEY
+        @Query("end_date") endDate: String
     ): Call<String>
 
     @GET("planetary/apod")
-    fun getPictureOfDay(
-        @Query("api_key") apiKey: String = Constants.API_KEY
-    ): Call<PictureOfDay>
+    fun getPictureOfDay(): Call<PictureOfDay>
 }
 
 private val moshi = Moshi.Builder()
@@ -32,11 +31,31 @@ private val moshi = Moshi.Builder()
     .build()
 
 object Network {
+    private val okHttpClient: OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(RequestInterceptor()).build()
     private val retrofit = Retrofit.Builder()
         .baseUrl(Constants.BASE_URL)
+        .client(okHttpClient)
         .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(MoshiConverterFactory.create(moshi))
         .build()
 
     val asteroids = retrofit.create(AsteroidsRest::class.java)
+}
+
+class RequestInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
+        val originalHttpUrl = originalRequest.url()
+
+        val newHttpUrl = originalHttpUrl.newBuilder()
+            .addQueryParameter("api_key", BuildConfig.NASA_API_KEY)
+            .build()
+
+        val newRequest = originalRequest.newBuilder()
+            .url(newHttpUrl)
+            .build()
+
+        return chain.proceed(newRequest)
+    }
 }
